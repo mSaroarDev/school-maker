b06dc1567606c82e87052f9d0d5c8f032551cda5
mSaroardev <msaroar.dev@gmail.com>
2025-08-26 23:43:37 +0600

    feat: implement LoginMain component with form inputs and image; enhance user experience for login functionality

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,16 @@
+import LoginMain from "./LoginMain";
+
+export const metadata = {
+  title: "Login - School Maker",
+  description: "Login to your School Maker account",
+};
+
+const page = () => {
+  return (
+    <>
+      <LoginMain />
+    </>
+  );
+};
+
+export default page;
