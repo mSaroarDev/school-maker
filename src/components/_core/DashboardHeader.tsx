77ea35c5541a1b31d099c8554b597b7f7c39480d
mSaroarDev <msaroar.dev@gmail.com>
2025-08-27 13:16:34 +0600

    feat: add UserDropdown component with avatar and dropdown menu functionality
    - Implemented UserDropdown component using Radix UI dropdown menu and avatar components.
    - Created Avatar, AvatarImage, and AvatarFallback components for user profile images.
    - Developed DropdownMenu and its related components for a customizable dropdown menu experience.
    - Included keyboard shortcuts for menu items and organized menu structure with groups and separators.

--- src/components/_core/DashboardHeader.tsx
+++ src/components/_core/DashboardHeader.tsx
@@ -1,3 +1,4 @@
+import UserDropdown from "./UserDropdown";
 
 type DashboardHeaderProps = {
   width: number;
@@ -8,8 +9,17 @@ const DashboardHeader = ({
 }: DashboardHeaderProps) => {
     return (
         <>
-          <div className="py-5" style={{marginLeft: `${width}px`, transition: 'margin-left 0.3s ease-in-out'}}>
-            Heder
+          <div 
+            className="py-2 flex items-center justify-end" 
+            style={{marginLeft: `${width}px`, transition: 'margin-left 0.3s ease-in-out'}}
+          >
+            <div className="flex items-center justify-end">
+              <div className="text-right">
+                <h2 className="font-semibold">Saroar Jahan</h2>
+                <p className="text-xs">Software Developer</p>
+              </div>
+              <UserDropdown />
+            </div>
           </div>
         </>
     );
