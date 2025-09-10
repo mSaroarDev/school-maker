ef2f63420a199a15082ff97e7b7cf13c8b2aa5ac
mSaroarDev <msaroar.dev@gmail.com>
2025-09-10 17:09:56 +0600

    feat: update TaskList component to include title and scrolling container

--- src/pages/calender/TaskList.tsx
+++ src/pages/calender/TaskList.tsx
@@ -1,13 +1,16 @@
 import Card from "@/components/ui/card";
 
 const TaskList = () => {
-    return (
-        <>
-          <Card className="mb-5">
-            Task List
-          </Card>
-        </>
-    );
+  return (
+    <>
+      <Card className="mb-5">
+        <h3 className="font-semibold text-lg">Tasks</h3>
+        <div className="mt-4 overflow-y-auto max-h-96">
+          
+        </div>
+      </Card>
+    </>
+  );
 };
 
 export default TaskList;
