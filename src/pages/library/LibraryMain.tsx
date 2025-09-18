34ba8f6172af6e70958735bd739f1d81e21a06fe
mSaroarDev <msaroar.dev@gmail.com>
2025-09-18 13:49:46 +0600

    refactor: clean up formatting and indentation in LibraryMain component

--- src/pages/library/LibraryMain.tsx
+++ src/pages/library/LibraryMain.tsx
@@ -4,24 +4,24 @@ import HeaderComponent from "@/components/_core/HeaderComponent";
 import Card from "@/components/ui/card";
 import { LibraryBreadTree } from "@/helpers/breadcrumbs";
 
-const LibraryMain = () => { 
-  
-    return (
-        <>
-          <BreadcrumbsComponent 
-            breadTree={LibraryBreadTree}
-          />
+const LibraryMain = () => {
 
-          <Card>
-            <HeaderComponent 
-              title="Library"
-              createButtonFunction={() => {}}
-              showSearch
-              searchPlaceholder="Search books..."
-            />
-          </Card>
-        </>
-    );
+  return (
+    <>
+      <BreadcrumbsComponent
+        breadTree={LibraryBreadTree}
+      />
+
+      <Card>
+        <HeaderComponent
+          title="Library"
+          createButtonFunction={() => { }}
+          showSearch
+          searchPlaceholder="Search books..."
+        />
+      </Card>
+    </>
+  );
 };
 
 export default LibraryMain;
