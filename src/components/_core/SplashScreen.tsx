d8ce7136dd2b6c2323e43119aca7023175530495
mSaroarDev <msaroar.dev@gmail.com>
2025-09-28 23:39:03 +0600

    feat: implement SplashScreen component and integrate it into SplashWrapper for improved loading experience

---
+++ src/components/_core/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import "@/assets/styles/SplashStyles.css";
+
+const SplashScreen = () => {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center bg-white">
+           <span className="loader"></span>
+        </div>
+    );
+};
+
+export default SplashScreen;
--- src/providers/SplashWrapper.tsx
+++ src/providers/SplashWrapper.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import airbnb from "@/assets/images/airbnb.svg";
 import Image from "next/image";
+import SplashScreen from "@/components/_core/SplashScreen";
 
 export default function SplashWrapper({ children }: { children: React.ReactNode }) {
   const [loading, setLoading] = useState(true);
@@ -17,28 +18,7 @@ export default function SplashWrapper({ children }: { children: React.ReactNode 
     <div>
       <AnimatePresence mode="wait">
         {loading ? (
-          <motion.div
-            key="splash"
-            className="fixed inset-0 flex items-center justify-center bg-white z-50"
-            initial={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.8 }}
-          >
-            <motion.h1
-              initial={{ scale: 0.8 }}
-              animate={{ scale: 1.1 }}
-              transition={{ repeat: Infinity, duration: 1, repeatType: "reverse" }}
-              className="text-xl font-bold"
-            >
-              <Image 
-                src={airbnb}
-                alt="Airbnb Logo"
-                width={100}
-                height={100}
-                className="w-24 h-24 fade-in"
-              />
-            </motion.h1>
-          </motion.div>
+          <SplashScreen />
         ) : (
           <motion.div
             key="content"
