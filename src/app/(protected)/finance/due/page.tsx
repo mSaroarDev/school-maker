c98388d2209e7227255fdf8205bac755bb766b32
mSaroarDev <msaroar.dev@gmail.com>
2025-10-04 17:08:16 +0600

    feat: add metadata and initial page structure for Due Fees dashboard

---
+++ src/app/(protected)/finance/due/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Due Fees - Finance`,
+  description: "Due Fees Dashboard",
+}
+
+const page = () => {
+    return (
+        <>
+            <h1>page</h1>
+        </>
+    );
+};
+
+export default page;
--- src/app/(protected)/finance/page.tsx
+++ src/app/(protected)/finance/page.tsx
@@ -1,4 +1,10 @@
 import FinanceMain from "@/views/finance/FinanceMain";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Dashboard - Finance`,
+  description: "Finance Dashboard",
+}
 
 const page = () => {
   return (
