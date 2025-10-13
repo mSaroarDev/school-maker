9a8581a926e58e65da3f2afc4f8edcc93120c76a
mSaroarDev <msaroar.dev@gmail.com>
2025-10-13 16:33:33 +0600

    refactor: comment out unused columns and CustomDataTable in FinanceMain component

--- src/views/finance/FinanceMain.tsx
+++ src/views/finance/FinanceMain.tsx
@@ -26,78 +26,78 @@ import ReviewModal from "./ReviewModal";
 const FinanceMain = () => {
   const [showReviewModal, setShowReviewModal] = useState(false);
 
-  const columns = [
-    {
-      name: "Student Name",
-      cell: (row) => (
-        <div className="flex items-center gap-1">
-          <div className="flex-shrink-0 w-10 h-10 overflow-hidden rounded-full bg-slate-50 relative">
-            <Image
-              src={""}
-              alt="Avatar"
-              fill
-            />
-          </div>
-          <div>
-            <h3 className="font-medium">{row?.studentName}</h3>
-            <p>{row?.studentId}</p>
-          </div>
-        </div>
-      )
-    },
-    {
-      name: "Class",
-      selector: (row) => row?.Class
-    },
-    {
-      name: "Tution Fee",
-      selector: (row) => row?.TutionFee
-    },
-    {
-      name: "Activity Fee",
-      selector: (row) => row?.activityFee
-    },
-    {
-      name: "Miscellenous Fee",
-      selector: (row) => row?.miscellenousFee
-    },
-    {
-      name: "Amount",
-      selector: (row) => row?.Amount
-    },
-    {
-      name: "Status",
-      cell: (row) => <RenderStatus status={row?.Status} />
-    },
-    {
-      name: "Action",
-      width: "100px",
-      cell: (row) => (
-        <DropdownMenu>
-          <DropdownMenuTrigger className="more-action-button">
-            <MdMoreVert size={20} />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>Action</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="cursor-pointer"
-              onClick={() => window.open(`/finance/reciept/${row?._id}`, "_blank")}
-            >
-              <FiEye size={18} /> View Reciept
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              className="cursor-pointer"
-              onClick={() => setShowReviewModal(true)}
-            >
-              <GrStreetView size={18} /> Review
-            </DropdownMenuItem>
+  // const columns = [
+  //   {
+  //     name: "Student Name",
+  //     cell: (row) => (
+  //       <div className="flex items-center gap-1">
+  //         <div className="flex-shrink-0 w-10 h-10 overflow-hidden rounded-full bg-slate-50 relative">
+  //           <Image
+  //             src={""}
+  //             alt="Avatar"
+  //             fill
+  //           />
+  //         </div>
+  //         <div>
+  //           <h3 className="font-medium">{row?.studentName}</h3>
+  //           <p>{row?.studentId}</p>
+  //         </div>
+  //       </div>
+  //     )
+  //   },
+  //   {
+  //     name: "Class",
+  //     selector: (row) => row?.Class
+  //   },
+  //   {
+  //     name: "Tution Fee",
+  //     selector: (row) => row?.TutionFee
+  //   },
+  //   {
+  //     name: "Activity Fee",
+  //     selector: (row) => row?.activityFee
+  //   },
+  //   {
+  //     name: "Miscellenous Fee",
+  //     selector: (row) => row?.miscellenousFee
+  //   },
+  //   {
+  //     name: "Amount",
+  //     selector: (row) => row?.Amount
+  //   },
+  //   {
+  //     name: "Status",
+  //     cell: (row) => <RenderStatus status={row?.Status} />
+  //   },
+  //   {
+  //     name: "Action",
+  //     width: "100px",
+  //     cell: (row) => (
+  //       <DropdownMenu>
+  //         <DropdownMenuTrigger className="more-action-button">
+  //           <MdMoreVert size={20} />
+  //         </DropdownMenuTrigger>
+  //         <DropdownMenuContent>
+  //           <DropdownMenuLabel>Action</DropdownMenuLabel>
+  //           <DropdownMenuSeparator />
+  //           <DropdownMenuItem
+  //             className="cursor-pointer"
+  //             onClick={() => window.open(`/finance/reciept/${row?._id}`, "_blank")}
+  //           >
+  //             <FiEye size={18} /> View Reciept
+  //           </DropdownMenuItem>
+  //           <DropdownMenuItem
+  //             className="cursor-pointer"
+  //             onClick={() => setShowReviewModal(true)}
+  //           >
+  //             <GrStreetView size={18} /> Review
+  //           </DropdownMenuItem>
 
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    }
-  ];
+  //         </DropdownMenuContent>
+  //       </DropdownMenu>
+  //     )
+  //   }
+  // ];
 
   return (
     <>
@@ -128,7 +128,7 @@ const FinanceMain = () => {
         />
 
         <div className="mt-5">
-          <CustomDataTable
+          {/* <CustomDataTable
             selectableRows
             columns={columns}
             data={transactions || []}
@@ -137,7 +137,7 @@ const FinanceMain = () => {
             noDataComponent="No books found"
             paginationComponent
             totalResults={transactions.length}
-          />
+          /> */}
         </div>
       </Card>
 
