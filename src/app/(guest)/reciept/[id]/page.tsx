96c41a7d8e58c44fbf0598e2bbd3d4768f6aae66
mSaroarDev <msaroar.dev@gmail.com>
2025-10-17 23:16:35 +0600

    feat: add metadata for receipt and finance dashboard pages

--- src/app/(guest)/reciept/[id]/page.tsx
+++ src/app/(guest)/reciept/[id]/page.tsx
@@ -1,4 +1,10 @@
 import InvoiceMain from "@/views/finance/InvoicePage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Receipt Details - ${process.env.NEXT_PUBLIC_APP_NAME}`,
+  description: `Receipt details page for ${process.env.NEXT_PUBLIC_APP_NAME} application`,
+}
 
 const page = () => {
   return (
--- src/app/(protected)/finance/[type]/page.tsx
+++ src/app/(protected)/finance/[type]/page.tsx
@@ -1,4 +1,11 @@
 import IncomeMain from "@/views/finance/IncomeMain";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `Dashboard - Finance`,
+  description: "Finance Dashboard",
+}
+
 
 const page = () => {
   return (
